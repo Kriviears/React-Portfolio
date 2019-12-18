42869ab13bac5b4d53bcee47b4c5e8ceff0737bd
Kriviears <kriviears@gmail.com>
2019-12-18 15:18:29 -0500

    static build

---
+++ precache-manifest.f620bb9ccf0d1713d8c9bff41153a762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8120d761fbe65032d73776e7320f58af",
+    "url": "/React-Portfolio/index.html"
+  },
+  {
+    "revision": "ccefc6af7d6deabb8d08",
+    "url": "/React-Portfolio/static/css/main.a94498f4.chunk.css"
+  },
+  {
+    "revision": "467f41377c427255f9de",
+    "url": "/React-Portfolio/static/js/2.f490f5ac.chunk.js"
+  },
+  {
+    "revision": "d0cb0a244b3563727e54b0f5d5982212",
+    "url": "/React-Portfolio/static/js/2.f490f5ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccefc6af7d6deabb8d08",
+    "url": "/React-Portfolio/static/js/main.e04a1360.chunk.js"
+  },
+  {
+    "revision": "f8d2580131a16c81d22e",
+    "url": "/React-Portfolio/static/js/runtime-main.dc0ed4e2.js"
+  }
+]);
